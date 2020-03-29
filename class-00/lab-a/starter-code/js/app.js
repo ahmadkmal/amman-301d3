615e332c92285081d43df9a3606833134e58d83b
ahmad kmal <60567529+ahmadkmal@users.noreply.github.com>
2020-03-29 15:03:14 +0300

    Revert "done 00"

--- class-00/lab-a/starter-code/js/app.js
+++ class-00/lab-a/starter-code/js/app.js
@@ -1,24 +1,24 @@
-`use strict`;
+'use strict';
 
-const names = [`bag`, `banana`, `bathroom`, `boots`, `breakfast`, `bubblegum`, `chair`, `cthulhu`, `dog-duck`, `dragon`, `pen`, `pet-sweep`, `scissors`, `shark`, `sweep`, `tauntaun`, `unicorn`, `usb`, `water-can`, `wine-glass`];
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-const leftImage = document.getElementById(`left`);
-const centerImage = document.getElementById(`center`);
-const rightImage = document.getElementById(`right`);
+var leftImage = document.getElementById('left');
+var centerImage = document.getElementById('center');
+var rightImage = document.getElementById('right');
 
-let allProducts = [];
-const container = document.getElementById(`image_container`);
-const viewed = [];
-const labels = [];
-const pics = [leftImage, centerImage, rightImage];
-const list = document.getElementById(`productlist`);
-let totalClicks = 0;
-const views = [];
-const votes = [];
+var allProducts = [];
+var container = document.getElementById('image_container');
+var viewed = [];
+var labels = [];
+var pics = [leftImage, centerImage, rightImage];
+var list = document.getElementById('productlist');
+var totalClicks = 0;
+var views = [];
+var votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = `img/${ name }.jpg`;
+  this.path = 'img/' + name + '.jpg';
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -30,17 +30,18 @@ function makeRandom() {
 
 function displayPics(){
   while(viewed.length < 6){
-    const rando = makeRandom();
+    var rando = makeRandom();
     while(!viewed.includes(rando)){
       viewed.push(rando);
     }
   }
-  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `const to `const`.
+  console.log(rando);
+  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
   // PUT YOUR RESPONSE IN THIS COMMENT
   console.log(viewed);
 
-  for (let i = 0; i < 3; i++){
-    const temp = viewed.shift();
+  for (var i = 0; i < 3; i++){
+    var temp = viewed.shift();
     pics[i].src = allProducts[temp].path;
     pics[i].id = allProducts[temp].name;
     allProducts[temp].views += 1;
@@ -48,20 +49,20 @@ function displayPics(){
 }
 
 function handleClick(event) {
-  if (event.target.id === `image_container`) {
-    return alert(`Be sure to click directly on an image!!`);
+  if (event.target.id === 'image_container') {
+    return alert('Be sure to click directly on an image!!');
   }
   totalClicks += 1;
   if(totalClicks > 24) {
-    container.removeEventListener(`click`, handleClick);
-    container.style.display = `none`;
+    container.removeEventListener('click', handleClick);
+    container.style.display = 'none';
     showList();
     makeChart();
   }
-  for(let i = 0; i < names.length; i++){
+  for(var i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(`${event.target.id } has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
+      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -70,9 +71,9 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(let i = 0; i < allProducts.length; i++) {
-    const liEl = document.createElement(`li`);
-    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`;
+  for(var i = 0; i < allProducts.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
     list.appendChild(liEl);
   }
 }
@@ -87,15 +88,15 @@ function makeChartData(){
 
 function makeChart(){
   makeChartData();
-  const ctx = document.getElementById(`chartypants`).getContext(`2d`);
+  var ctx = document.getElementById('chartypants').getContext('2d');
   new Chart(ctx, { //eslint-disable-line
-    type: `bar`,
+    type: 'bar',
     data: {
       labels: labels,
       datasets: [{
-        label: `total votes`,
-        backgroundColor: `gold`,
-        borderColor: `#214`,
+        label: 'total votes',
+        backgroundColor: 'gold',
+        borderColor: '#214',
         data: votes,
       }]
     },
@@ -112,22 +113,22 @@ function makeChart(){
       }
     }
   });
-  Chart.defaults.global.defaultFontColor = `#eee`; //eslint-disable-line
+  Chart.defaults.global.defaultFontColor = '#eee'; //eslint-disable-line
 }
 
-container.addEventListener(`click`, handleClick);
+container.addEventListener('click', handleClick);
 
-document.getElementById(`bus`).addEventListener(`click`, function(){
-  localStorage.removeItem(`busmall`);
-  console.log(`Local storage was cleared!`);
+document.getElementById('bus').addEventListener('click', function(){
+  localStorage.removeItem('busmall');
+  console.log('Local storage was cleared!');
 });
 
 if(localStorage.busmall){
-  console.log(`Local storage data exists`);
+  console.log('Local storage data exists');
   allProducts = JSON.parse(localStorage.busmall);
 } else {
-  console.log(`There is no local storage data; initialize app by creating instances`);
-  for(let i = 0; i < names.length; i++) {
+  console.log('There is no local storage data; initialize app by creating instances');
+  for(var i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
 }
